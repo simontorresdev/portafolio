c78cce1d6788bcce2cfa890cdf25700ecf8be516
Simon Torres <simontorres979797@gmail.com>
2020-07-15 21:10:40 -0500

    probando velocidad2

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
-// import Header from '../Header'
+import Header from '../Header'
 import { GlobalStyles } from '../../styles/GlobalStyles'
 
 const Layout = ({ children }) => {
   return (
     <>
       <GlobalStyles />
-      {/* <Header /> */}
+      <Header />
       <main>{children}</main>
     </>
   )
