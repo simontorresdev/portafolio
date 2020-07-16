3f11e83e9e1a479452e87b69813b4a9e225a648c
Simon Torres <simontorres979797@gmail.com>
2020-07-15 22:17:26 -0500

    probando velocidad7

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,12 +1,8 @@
 const React = require('react')
-const { GlobalStyles } = require('./src/styles/GlobalStyles')
 const Layout = require('./src/components/Layout').default
 
 exports.wrapRootElement = ({ element }) => (
-  <>
-    <GlobalStyles />
-    <Layout>
-      {element}
-    </Layout>
-  </>
+  <Layout>
+    {element}
+  </Layout>
 )
