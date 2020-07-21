a5379eef4f2cbac4c7335167c0fb5057e626714d
Simon Torres <simontorres979797@gmail.com>
2020-07-20 21:47:55 -0500

    agregando fuente

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Header from '../Header'
+import { Helmet } from 'react-helmet'
 import { GlobalStyles } from '../../styles/GlobalStyles'
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Helmet>
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Montserrat&display=swap'
+          rel='stylesheet'
+        />
+      </Helmet>
       <GlobalStyles />
       <Header />
       <main>{children}</main>
